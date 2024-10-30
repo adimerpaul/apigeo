c2b7f89e2ae025b18415598b5117558825df61ac
adimer <adimer101@gmail.com>
2024-10-29 21:26:50 -0400

    creado rutas de locations

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Location;
 use Illuminate\Http\Request;
 
-class LocationController extends Controller
-{
-    //
+class LocationController extends Controller{
+    public function index(){
+        return Location::all();
+    }
 }
--- app/Models/Location.php
+++ app/Models/Location.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Location extends Model
-{
-    //
+class Location extends Model{
+    use SoftDeletes;
+    protected $fillable = [
+        'name',
+        'address',
+        'city',
+        'latitude',
+        'longitude',
+    ];
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
